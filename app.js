ded95d31c4c0430f185cfc6af7be80229b197d50
ACERLEANDRO\leo_d <leo1996.dasilva@gmail.com>
2022-11-12 21:30:06 -0300

    EX8

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-function obterClienteNoBancoDeDados(idCliente) {
-  return new Promise( (resolve, reject) => {
-          const cliente = { nome: 'bruce wayne', id: idCliente };
-          resolve(cliente);
+const mainUsuarios = document.getElementById("mainUsuarios")
+
+async function getUsuarios(quantidade = 4) {
+  const requisicao = await fetch("https://randomuser.me/api/?results=" + quantidade)
+  const body = await requisicao.json()
+  montaHtmlUsuarios(body)
+}
+
+const montaHtmlUsuarios = (body) => {
+  mainUsuarios.innerHTML = ""
+  body.results.forEach(usuario => {
+    mainUsuarios.appendChild(montaLiUsuario(usuario));
   });
 }
 
-async function processar() {
-  const usuarioBanco = await obterClienteNoBancoDeDados(7);
-  console.log(usuarioBanco);
+const montaLiUsuario = (usuario) => {
+  const liUsuario = document.createElement("li")
+  liUsuario.style = "background: aqua;"
+
+  const nome = document.createElement("h2")
+  const {title, first, last} = usuario.name
+  nome.innerText = title + " "
+  nome.innerText += first + " "
+  nome.innerText += last
+  liUsuario.appendChild(nome)
+
+  const foto = document.createElement("img")
+  foto.src = usuario.picture.large
+  liUsuario.appendChild(foto)
+
+  const endereco = document.createElement("p")
+  const {street, city, state, country} = usuario.location
+  endereco.innerText = street.name + " "
+  endereco.innerText += city + " "
+  endereco.innerText += state + " "
+  endereco.innerText += country
+  liUsuario.appendChild(endereco)
+  return liUsuario
 }
 
-processar()
+getUsuarios(3)
+
+const botaoQuantidadeUsuarios = document.getElementById("quantidadeUsuarios")
+botaoQuantidadeUsuarios.addEventListener('change', (event) => getUsuarios(event.target.value))
